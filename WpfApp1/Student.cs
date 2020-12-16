176b31a70931a8fb52a37894ce48e793f47f2be5
Mark Durkin <S00198967@mail.itsligo.ie>
2020-12-16 15:47:55 +0000

    Add project files.

---
+++ WpfApp1/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Student(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+       
+        }
+
+        public override string ToString()
+        {
+            { 
+                return string.Format($"{LastName}, {FirstName}");
+                }
+        }
+    }
+}
